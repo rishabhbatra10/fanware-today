8573768d3d4bb1a5f18a5c9b9dee21e71cc0f970
Rishabh Batra <rishabh@pankhuri.co>
2020-04-18 19:16:21 +0530

    Added overlay css

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -153,6 +153,29 @@ export default {
 };
 </script>
 <style>
+
+.v-overlay--absolute {
+  position: absolute
+}
+
+.v-overlay--active {
+  pointer-events: auto
+}
+
+.v-overlay{
+  align-items: center;
+  border-radius: inherit;
+  display: flex;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  pointer-events: none;
+  transition: 0.3s cubic-bezier(0.25, 0.8, 0.5, 1), z-index 1ms;
+}
+
 .v-overlay__scrim {
   border-radius: inherit;
   bottom: 0;
